12d4f11ecb6df2beaed6d9cc84197746eece0643
eduardomarra <eduardomarra@gmail.com>
2024-04-12 17:02:22 -0300

    create Desafio Modelo Dominio ORM project and classes

---
+++ modeloDominioORM/src/main/java/com/desafioModeloDominioORM/modeloDominioORM/entities/Atividade.java
@@ -0,0 +1,27 @@
+package com.desafioModeloDominioORM.modeloDominioORM.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    @Column(columnDefinition = "TEXT")
+    private String descricao;
+    private Double price;
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+    @ManyToMany(mappedBy = "atividades")
+    private Set<Participante> participantes = new HashSet<>();
+
+    @OneToMany(mappedBy = "atividade")
+    private Set<Bloco> blocos = new HashSet<>();
+}
