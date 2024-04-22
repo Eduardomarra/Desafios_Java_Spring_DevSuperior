cfa911919639168c500dd10afcecbc3605328204
eduardomarra <eduardomarra@gmail.com>
2024-04-22 12:51:35 -0300

    Added ClientRepository and ServiceClient

--- crudClient/src/main/java/com/emarra/crudClient/repositories/ClientRepository.java
+++ crudClient/src/main/java/com/emarra/crudClient/repositories/ClientRepository.java
@@ -1,2 +1,7 @@
-package com.emarra.crudClient.repositories;public class ClientRepository {
+package com.emarra.crudClient.repositories;
+
+import com.emarra.crudClient.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
--- crudClient/src/main/java/com/emarra/crudClient/services/ClientService.java
+++ crudClient/src/main/java/com/emarra/crudClient/services/ClientService.java
@@ -1,2 +1,32 @@
-package com.emarra.crudClient.services;public class ClientService {
+package com.emarra.crudClient.services;
+
+import com.emarra.crudClient.dto.ClientDTO;
+import com.emarra.crudClient.entities.Client;
+import com.emarra.crudClient.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById( Long id) {
+        Optional<Client> client = repository.findById(id);
+        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
 }
