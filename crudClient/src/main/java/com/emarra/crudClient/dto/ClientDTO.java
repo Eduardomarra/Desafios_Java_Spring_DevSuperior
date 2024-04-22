8accdd9d5df1013a431ba147b696ecebe8da8a08
eduardomarra <eduardomarra@gmail.com>
2024-04-22 12:52:28 -0300

    Added ClientDTO

--- crudClient/src/main/java/com/emarra/crudClient/dto/ClientDTO.java
+++ crudClient/src/main/java/com/emarra/crudClient/dto/ClientDTO.java
@@ -1,2 +1,67 @@
-package com.emarra.crudClient.dto;public class ClientDTO {
+package com.emarra.crudClient.dto;
+
+import com.emarra.crudClient.entities.Client;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class ClientDTO {
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client client) {
+        id = client.getId();
+        name = client.getName();
+        cpf = client.getCpf();
+        income = client.getIncome();
+        birthDate = client.getBirthDate();
+        children = client.getChildren();
+    }
+
+    public ClientDTO(Optional<Client> client) {
+        id = client.get().getId();
+        name = client.get().getName();
+        cpf = client.get().getCpf();
+        income = client.get().getIncome();
+        birthDate = client.get().getBirthDate();
+        children = client.get().getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
 }
